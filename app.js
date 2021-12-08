156c53c8f18545862c7b8c0134488c131e0cd17d
code_max <ogbujigodwinmax@gmail.com>
2021-12-08 15:49:24 +0100

    Set up of the project

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require("morgan");
 
+const globalErrorHandler = require('./controllers/errorHandler')
+
 const app = express();
 
 app.use(express.json());
@@ -10,10 +12,13 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handling unavailable routes
 app.all("*", (req, res, next) => {
   res.status(404).json({
       message: "route not found"
   });
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ models/userModel.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "A user must have a name"],
+        minlength: [5, "Full name must be more than five(5) characters"]
+    },
+    email: {
+        type: String,
+        required: [true, 'A user must have an email'],
+        unique: true,
+        validate: [validator.isEmail, 'Enter a valid email format'],
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minLength: [6, 'Password must be more than five (5) characters'],
+        select: false
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // Only works on save and create
+            validator: function(el) {
+                return this.password === el;
+            },
+            message: 'Passwords don\'t match'
+        }
+    },
+    photo: {
+        type: String,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'vendor', 'admin'],
+        default: 'user'
+    },
+    phone: {
+        type: String,
+        required : [true, 'A user must have phone number'],
+        maxlength: [14, 'Enter a valid phone number'],
+        minlength: [7, 'Enter a valid number'],
+        validate: {
+            validator: function (el) {
+                const values = ['+', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+                const elements = el.split('');
+                elements.forEach(element => {
+                    if (!values.includes(element)) return false;
+                });
+            },
+            message: 'Enter a valid phone number'
+        }
+    },
+    status: {
+        type: String,
+        default: 'offline',
+        enum: ['online', 'offline']
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+},
+{
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+}
+)
+
+const User = mongoose.model('User', userSchema);
+
+module.export = User;
